a657ccf6086d5c579439452a354e713841709607
Muhammad Saad Tehseen <saadtehseen96@gmail.com>
2022-11-26 22:30:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def fill_form():  
+    return '''  
+        <h1>Call the API and Get the data</h1>  
+        <form method="post" action="/getdata">  
+             
+            <input type="submit" value = "Click Here To Fetch API Data" >  
+        </form>   
+        ''' 
